ae596afe4700ab9dc5711ef1259abd5a67bd9913
starminalush <alica154323@gmail.com>
2024-05-01 23:09:07 +0500

    add services for analytics

--- tg_bot/main.py
+++ tg_bot/main.py
@@ -1,15 +1,38 @@
 import os
 
+from dff import Context
 from dff.context_storages import context_storage_factory
 from dff.messengers.telegram import PollingTelegramInterface
-from dff.pipeline import Pipeline
+from dff.pipeline import Pipeline, ExtraHandlerRuntimeInfo
 from dialog_graph import script
+from dff.stats import (
+    OtelInstrumentor,
+    set_logger_destination,
+    set_tracer_destination,
+)
+from dff.stats import OTLPLogExporter, OTLPSpanExporter
 
+
+set_logger_destination(OTLPLogExporter("grpc://otelcol:4317", insecure=True))
+set_tracer_destination(OTLPSpanExporter("grpc://otelcol:4317", insecure=True))
+dff_instrumentor = OtelInstrumentor()
+dff_instrumentor.instrument()
+
+
+# example extractor function
+@dff_instrumentor
+async def get_service_state(ctx: Context, _, info: ExtraHandlerRuntimeInfo):
+    # extract execution state of service from info
+    data = {
+        "execution_state": info.component.execution_state,
+    }
+    # return a record to save into connected database
+    return data
 
 def get_pipeline() -> Pipeline:
     telegram_token = os.getenv("TG_BOT_TOKEN")
 
-    db_uri = "postgresql+asyncpg://{}:{}@0.0.0.0:5432/{}".format(
+    db_uri = "postgresql+asyncpg://{}:{}@postgres:5432/{}".format(
         os.environ["POSTGRES_USERNAME"],
         os.environ["POSTGRES_PASSWORD"],
         os.environ["POSTGRES_DB"],
@@ -31,7 +54,7 @@ def get_pipeline() -> Pipeline:
         start_label=("general_flow", "start_node"),
         fallback_label=("general_flow", "fallback_node"),
         messenger_interface=messenger_interface,
-        context_storage=db
+        context_storage=db,
     )
 
     return pipeline
